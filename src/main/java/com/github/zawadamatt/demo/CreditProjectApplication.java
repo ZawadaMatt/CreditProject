138e789766b0f911619661afdf0d98df0098ad96
Mateusz Zawada <mateusz.zawada10@gmail.com>
2019-11-29 11:21:06 +0100

    Add spring support

---
+++ src/main/java/com/github/zawadamatt/demo/CreditProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.zawadamatt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditProjectApplication.class, args);
+	}
+
+}
