32a080dc8e36190547b241baa7e1860e82eb93b7
zawadamatt <mateusz.zawada10@gmail.com>
2019-12-05 08:56:49 +0100

    JdbcConfig refactor

--- src/main/java/com/github/zawadamatt/demo/config/JdbcConfig.java
+++ src/main/java/com/github/zawadamatt/demo/config/JdbcConfig.java
@@ -13,9 +13,10 @@ public class JdbcConfig {
     @Bean
     public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url("jdbc:mysql://127.0.0.1:3306?serverTimezone=UTC");
-        dataSourceBuilder.username("root");
-        dataSourceBuilder.password("qwerty");
+        dataSourceBuilder.url("jdbc:mysql://127.0.0.1:3306/nazwabazydanych?serverTimezone=UTC"); //adres servera bazy danych, ustawienie strefy czasowej
+        dataSourceBuilder.username("nazwa uzytkownika"); //nazwa użytkownika
+        dataSourceBuilder.password("haslo"); //hasło do bazy danych,
+        // dataSourceBuilder.driverClassName() (opcjonalnie dodać sterownik bazy danych
         return dataSourceBuilder.build();
     }
 
